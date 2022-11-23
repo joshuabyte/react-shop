840e1095a624c0853e00bdd92d7d1e189db18dbe
protondk <edgarjosue2911@gmail.com>
2022-11-23 15:29:03 -0600

    Initialized React Project

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import React from "react";
+//Careful with the brackets {} below. If there's an error, remove them.
+import { ReactDOM } from "react";
+
+ReactDOM.render(<App />, document.getElementById("app"));
