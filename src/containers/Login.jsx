29a9dffa3232c4e7c5df1c53dc0ec77b5e5ef22e
protondk <edgarjosue2911@gmail.com>
2022-11-26 23:12:49 -0600

    Login component is working. Mobile logo is not showing. Will try to fix for next commit

--- src/containers/Login.jsx
+++ src/containers/Login.jsx
@@ -1,27 +1,48 @@
-import React from 'react';
+import React from "react";
+import "../styles/Login.scss";
 
 const Login = () => {
-    return (
-        <div className="login">
-        <div className="form-container">
-          <img src="./logos/logo_yard_sale.svg" alt="logo" className="logo" />
-    
-          <h1 className="title">Create a new password</h1>
-          <p className="subtitle">Enter a new passwrd for yue account</p>
-    
-          <form action="/" className="form">
-            <label for="password" className="label">Password</label>
-            <input type="password" id="password" placeholder="*********" className="input input-password" />
-    
-            <label for="new-password" className="label">Password</label>
-            <input type="password" id="new-password" placeholder="*********" className="input input-password" />
-    
-            <input type="submit" value="Confirm" className="primary-button login-button" />
-          </form>
-        </div>
+  return (
+    // In React, class becomes className
+    <div className="login">
+      <div className="form-container">
+        <img src="./logos/logo_yard_sale.svg" alt="logo" className="logo" />
+
+        <h1 className="title">Create a new password</h1>
+        <p className="subtitle">Enter a new passwrd for yue account</p>
+
+        <form action="/" className="form">
+          {/* In React, for becomes htmlFor */}
+          <label htmlFor="password" className="label">
+            Password
+          </label>
+          <input
+            type="password"
+            id="password"
+            placeholder="*********"
+            className="input input-password"
+          />
+
+          {/* In React, for becomes htmlFor */}
+          <label htmlFor="new-password" className="label">
+            Password
+          </label>
+          <input
+            type="password"
+            id="new-password"
+            placeholder="*********"
+            className="input input-password"
+          />
+
+          <input
+            type="submit"
+            value="Confirm"
+            className="primary-button login-button"
+          />
+        </form>
       </div>
-    );
-}
+    </div>
+  );
+};
 
 export default Login;
-
